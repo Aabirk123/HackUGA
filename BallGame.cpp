704c9163253a80f797faf66f55da3a5aae76d27d
ccuser-at-866423725286 <ccuser-at-866423725286>
2020-02-07 23:35:18 -0500

    stuff

--- BallGame.cpp
+++ BallGame.cpp
@@ -31,61 +31,64 @@ int main(int argc, char *argv[])
     SDL_Event e;
 
     //While application is running
-    SDL_Rect dest = {200, 300};
+    SDL_Rect dest = {200, -300};
     int xVelo = 0;
     while( !quit )
     {
-        //Handle events on queue
-        while( SDL_PollEvent( &e ) != 0 )
+      
+      //Handle events on queue
+      while( SDL_PollEvent( &e ) != 0 )
         {
-            //User requests quit
-            if( e.type == SDL_QUIT )
+	  //User requests quit
+	  
+	  if( e.type == SDL_QUIT )
             {
-                quit = true;
+	      quit = true;
             } //User presses a key
-            else if( e.type == SDL_KEYDOWN )
+	  else if( e.type == SDL_KEYDOWN )
             {
-                switch( e.key.keysym.sym )
+	      switch( e.key.keysym.sym )
                 {
-                    case SDLK_ESCAPE:
-                        quit = true;
-                    break;
-
-                    case SDLK_LEFT:
-                        xVelo = -1;
-                    break;
-
-                    case SDLK_RIGHT:
-                        xVelo = 1;
-                    break;
+		case SDLK_ESCAPE:
+		  quit = true;
+		  break;
+		  
+		case SDLK_LEFT:
+		  xVelo = -1;
+		  break;
+		  
+		case SDLK_RIGHT:
+		  xVelo = 1;
+		  break;
                 }
             } else if( e.type == SDL_KEYUP) {
-                switch( e.key.keysym.sym )
-                {
-                    case SDLK_LEFT:
-                        xVelo = 0;
-                    break;
-
-                    case SDLK_RIGHT:
-                        xVelo = 0;
-                    break;
-                }
-            }
-        }
-
+	    switch( e.key.keysym.sym )
+	      {
+	      case SDLK_LEFT:
+		xVelo = 0;
+		break;
+		      
+	      case SDLK_RIGHT:
+		xVelo = 0;
+		break;
+	      }
+	  }
+	  
+	}
+      
         // Move player image's position
         dest.x += xVelo;
         //Apply the image
         SDL_BlitSurface( gXOut, NULL, ScreenSurface, &dest );
-    
+	
         //Update the surface
         SDL_UpdateWindowSurface( Window );
         SDL_Delay(5);
     }
-
-	//Deallocate surface
+    
+    //Deallocate surface
 	SDL_FreeSurface( gXOut );
 	SDL_DestroyWindow( Window );
-
-    return 0;
+	
+	return 0;
 }
