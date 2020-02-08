c16ecb9bec187bf1ed3028c328d5beae3d6ad746
ccuser-at-866423725286 <ccuser-at-866423725286>
2020-02-08 11:59:46 -0500

    ball.h

--- ball.h
+++ ball.h
@@ -4,37 +4,10 @@
 class Ball
 {
 protected:
-  int x;
-  int y;
-  SDL_Surface* sprite;
+    int x;
+    int y;
+    SDL_Surface* sprite;
 public:
-<<<<<<< HEAD
-  //Parametrized Constructor
-  Ball(int x, int color) {
-    this->x = x;
-    this->y = -10;
-    std::string text = "ball" + std::to_string(color) + ".bmp";
-    sprite = SDL_LoadBMP( text.c_str() );
-  }
-  
-  void Update(int speed) {
-      y += speed;
-  }
-  
-  bool touchingBox(int targetX, int targetY, int width, int height) {
-    if((x > targetX && x < targetX+width) || (x+40 > targetX && x+40 < targetX+width))
-      {
-	if((y > targetY && y < targetY+height) || (y+40 > targetY && y+40 < targetY+height))
-	  return true;
-      }
-    return false;
-    }
-  void Paste(SDL_Surface* ScreenSurface) {
-    SDL_Rect dest = {x, y};
-    SDL_BlitSurface( sprite, NULL, ScreenSurface, &dest );
-  }
-};
-=======
     int color;
     //Parametrized Constructor
     Ball(int x, int color) {
@@ -68,4 +41,3 @@ public:
         SDL_BlitSurface( sprite, NULL, ScreenSurface, &dest );
     }
 };
->>>>>>> c03a24f3650100536435b7fb058b9a36c8ca6fb9
