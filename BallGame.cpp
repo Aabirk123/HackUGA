a9e549a8c2ae2ba90874d8c36c3df13d7a88696e
kingoftheconnors <Connormcp777@gmail.com>
2020-02-07 23:27:53 -0500

    Added falling ball object

--- BallGame.cpp
+++ BallGame.cpp
@@ -1,12 +1,13 @@
 #include<SDL2/SDL.h>
 #include<cstdio>
+#include "ball.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char *argv[])
 {
-
+    //srand(time(NULL));
     // Start SDL
     SDL_Init(SDL_INIT_EVERYTHING);
     // Setup the screen
@@ -18,6 +19,11 @@ int main(int argc, char *argv[])
 
     // Import image
     SDL_Surface* gXOut = SDL_LoadBMP( "byte.bmp" );
+
+    SDL_Surface* crator = SDL_LoadBMP( "crater1.bmp" );
+    SDL_Surface* crator2 = SDL_LoadBMP( "crater2.bmp" );
+    SDL_Surface* crator3 = SDL_LoadBMP( "crater3.bmp" );
+    SDL_Surface* crator4 = SDL_LoadBMP( "crater4.bmp" );
 	if( gXOut == NULL )
 	{
 		printf( "Unable to load image %s! SDL Error: %s\n", "03_event_driven_programming/x.bmp", SDL_GetError() );
@@ -32,9 +38,34 @@ int main(int argc, char *argv[])
 
     //While application is running
     SDL_Rect dest = {200, 300};
+    SDL_Rect C1 = {};
+    SDL_Rect C2 = {};
+    SDL_Rect C3 = {};
+    SDL_Rect C4 = {};
+    
     int xVelo = 0;
+    //int xtime = 0;
+    int lvlDifficulty = 2;
+
+    Ball* testBall[100];
+    for(int i = 0; i <= lvlDifficulty; i++)
+    {
+        testBall[i] = new Ball(200+i*50, i);
+    }
+
     while( !quit )
     {
+        //Timer for dropping fall
+        /*int timePassed = SDL_GetTicks();
+        if(timePassed % 600 == 0)
+        {
+            xtime = -1;
+        }*/
+        for(int i = 0; i <= lvlDifficulty; i++)
+        {
+            testBall[i]->Update(1);
+            testBall[i]->Paste(ScreenSurface);
+        }
         //Handle events on queue
         while( SDL_PollEvent( &e ) != 0 )
         {
